624688f4a8d60631627e5b28c4080644b58c8361
Casa <alonsoriano2007@gmail.com>
2026-09-17 14:44:05 +0200

    Script clase

---
+++ Acceso a datos/Primeros ejercicios/Ejercicios/src/Urgencias/ComparadorPacientes.java
@@ -0,0 +1,17 @@
+package Urgencias;
+
+import java.util.Comparator;
+
+public class ComparadorPacientes implements Comparator<Paciente> {
+    @Override
+    public int compare(Paciente o1, Paciente o2) {
+//        if (o1.getGravedad() == o2.getGravedad()){
+//            return o1.getHoraDeLegada().isBefore(o2.getHoraDeLegada()) ? 1 : -1;
+//        } else {
+//            return o1.getGravedad() - o2.getGravedad();
+//        }
+        // Reducido
+        return o1.getGravedad() == o2.getGravedad() ? (o1.getHoraDeLegada().isAfter(o2.getHoraDeLegada()) ? 1 : -1) : o2.getGravedad() - o1.getGravedad();
+
+    }
+}
---
+++ Acceso a datos/Primeros ejercicios/Ejercicios/src/Urgencias/Paciente.java
@@ -0,0 +1,52 @@
+package Urgencias;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Paciente {
+    private String Nombre;
+    private Integer gravedad;
+    private LocalDateTime horaDeLegada;
+
+    public Paciente(String nombre, Integer gravedad, LocalDateTime horaDeLegada) {
+        Nombre = nombre;
+        this.gravedad = gravedad;
+        this.horaDeLegada = horaDeLegada;
+    }
+
+    public Paciente() {
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Integer getGravedad() {
+        return gravedad;
+    }
+
+    public void setGravedad(Integer gravedad) {
+        this.gravedad = gravedad;
+    }
+
+    public LocalDateTime getHoraDeLegada() {
+        return horaDeLegada;
+    }
+
+    public void setHoraDeLegada(LocalDateTime horaDeLegada) {
+        this.horaDeLegada = horaDeLegada;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "Nombre='" + Nombre + '\'' +
+                ", gravedad=" + gravedad +
+                ", horaDeLegada=" + horaDeLegada +
+                '}';
+    }
+}
